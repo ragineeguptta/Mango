517c56f1fcad887cc6e44b1a614ed2b9402826ee
Raginee Gupta <ragineegupta123@gmail.com>
2026-04-23 15:00:26 +0530

    Create Auth API and NuGet Packages 1:43 Add DbContext and Create Identity Tables 8:30 Add Custom Properties to User Table 3:39 Endpoints for Login and Register 1:39 Add DTO's 4:13 Configure JwtOptions 3:28 IAuth Service 3:54 Register Endpoint in Auth Service 3:34 Register in Action 4:44 Login in Action 4:13 Generate Jwt Token 7:45 Token in Action 3:39 Assign Role

---
+++ Mango.Services.AuthAPI/Controllers/AuthAPIController.cs
@@ -0,0 +1,68 @@
+﻿using Mango.Services.AuthAPI.Models.Dto;
+using Mango.Services.AuthAPI.Service.IService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.AuthAPI.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthAPIController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        protected ResponseDto _response;
+        public AuthAPIController(IAuthService authService)
+        {
+            _authService = authService;
+            _response = new();
+        }
+
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegistrationRequestDto model)
+        {
+            var errorMessage = await _authService.Register(model);
+            if (!string.IsNullOrEmpty(errorMessage))
+            {
+                _response.IsSuccess = false;
+                _response.Message = errorMessage;
+                return BadRequest(_response);
+            }
+
+            _response.Message = "User registered successfully";
+            return Ok(_response);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto model)
+        {
+            
+            var loginResponse = await _authService.Login(model);
+            if (loginResponse == null)
+            {
+                _response.IsSuccess = false;
+                _response.Message = "Username or password is incorrect";
+                return BadRequest(_response);
+            }
+            _response.Result = loginResponse;
+            _response.Message = "Loging Successfully";
+            return Ok(_response);
+        }
+
+        [HttpPost("AssignRole")]
+        public async Task<IActionResult> AssignRole([FromBody] RegistrationRequestDto model)
+        {
+
+            var assignRoleSucc = await _authService.AssignRole(model.Email, model.Role.ToUpper());
+            if (!assignRoleSucc)
+            {
+                _response.IsSuccess = false;
+                _response.Message = "Failed to assign role";
+                return BadRequest(_response);
+            }
+
+            _response.Message = "Role assigned successfully";
+            return Ok(_response);
+        }
+    }
+}
